3010bfa6b4b1122965ff9d5ec038c9ee36ce8d13
ydh91 <mhyi91@outlook.com>
2026-08-07 17:48:57 +0800

    fix: restore Jest test configuration after Vite migration
    Rewrite jest.config.js to remove Craco dependency; add moduleNameMapper for path aliases; use babel-jest with new babel.config.js; use setupFilesAfterEnv for setupTests.ts; add CSS and static asset mock files

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -16,9 +16,66 @@
  * limitations under the License.
  */
 
-const { createJestConfig } = require('@craco/craco');
+const path = require('path');
 
-const cracoConfig = require('./craco.config.js');
-const jestConfig = createJestConfig(cracoConfig, {});
-
-module.exports = jestConfig;
+module.exports = {
+  rootDir: path.resolve(__dirname),
+  testEnvironment: 'jsdom',
+  setupFiles: ['jest-canvas-mock'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: ['<rootDir>/src/**/__tests__/**/*.{spec,test}.{js,jsx,ts,tsx}'],
+  moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'web.tsx', 'tsx', 'json', 'web.jsx', 'jsx', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(echarts|zrender|react-monaco-editor|monaco-editor|monaco-editor-webpack-plugin)/)',
+  ],
+  moduleNameMapper: {
+    // Path aliases matching tsconfig.json
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@locales/(.*)$': '<rootDir>/src/locales/$1',
+    '^@redux-store/(.*)$': '<rootDir>/src/redux/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^components/(.*)$': '<rootDir>/src/app/components/$1',
+    '^pages/(.*)$': '<rootDir>/src/app/pages/$1',
+    '^hooks/(.*)$': '<rootDir>/src/app/hooks/$1',
+    '^slices/(.*)$': '<rootDir>/src/app/slice/$1',
+    '^locales/(.*)$': '<rootDir>/src/locales/$1',
+    '^redux/(.*)$': '<rootDir>/src/redux/$1',
+    '^styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^core/(.*)$': '<rootDir>/src/app/models/$1',
+    '^migration/(.*)$': '<rootDir>/src/app/migration/$1',
+    '^types/(.*)$': '<rootDir>/src/app/types/$1',
+    '^types$': '<rootDir>/src/types.ts',
+    '^entryPointFactory$': '<rootDir>/src/entryPointFactory',
+    '^globalConstants$': '<rootDir>/src/globalConstants',
+    // Handle antd's ~-prefixed less imports
+    '^~antd/(.*)$': '<rootDir>/node_modules/antd/$1',
+    '^~@ant-design/(.*)$': '<rootDir>/node_modules/@ant-design/$1',
+    // Mock static assets
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png|jpg|jpeg)$': '<rootDir>/src/__mocks__/fileMock.js',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*/*.d.ts',
+    '!src/**/*/Loadable.{js,jsx,ts,tsx}',
+    '!src/**/*/messages.ts',
+    '!src/**/*/types.ts',
+    '!src/index.tsx',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  coverageReporters: ['html', 'lcov', 'text-summary'],
+};
--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,4 +1,4 @@
-﻿import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme from 'enzyme';
 import 'locales/i18n';
